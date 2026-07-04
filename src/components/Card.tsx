5b540629519a830fee368537b7dcd9ace458680c
mazares <stefan_mazare@yahoo.com>
2026-07-04 16:41:25 +0300

    fiexed the card id bug

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,8 @@
-export default function Card({ name, id }: { name: string; id: number }) {
+export default function Card({ name, id: _id }: { name: string; id: number }) {
   return (
     <div className="p-2 border">
       <h2>{name}</h2>
-      {/*<p>{id}</p>*/}
+      {/*<p>{_id}</p>*/}
     </div>
   );
 }
